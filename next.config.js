eba18e6e3c3c766ac2dc6937bf4e4435ac9f37bd
Tholut Akhyar <tholutx@gmail.com>
2023-03-03 15:27:34 +0700

    Add i.ibb.co domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'avatars.githubusercontent.com'],
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'i.ibb.co',
+    ],
   },
 }
